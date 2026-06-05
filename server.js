091c959eae3c7c8996cb6a982dafd810e4d243e6
Ashish Biswas <biswasashish655@gmail.com>
2026-06-05 13:03:37 +0530

    iniital commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
+process.on("uncaughtException", (err) => {
+ console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+ console.log(err.name, err.message);
+ process.exit(1);
+});
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+ "<PASSWORD>",
+ process.env.DATABASE_PASSWORD,
+);
+
+const clientOptions = {
+ serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function connectDB() {
+ try {
+  await mongoose.connect(DB, clientOptions);
+  console.log("DB connection successful!");
+ } catch (err) {
+  console.log("DB connection error:", err);
+  process.exit(1);
+ }
+}
+connectDB();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+ console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+ console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+ console.log(err.name, err.message);
+ server.close(() => {
+  process.exit(1);
+ });
+});
